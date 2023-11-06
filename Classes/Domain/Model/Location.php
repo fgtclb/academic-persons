da3c6707678e8859079485454131bbbc566a2cd6
Tim Schreiner <tim.schreiner@km2.de>
2023-11-06 13:50:31 +0100

    [TASK] Add domain model for location records

---
+++ Classes/Domain/Model/Location.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "academic_persons" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Fgtclb\AcademicPersons\Domain\Model;
+
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+class Location extends AbstractEntity
+{
+    protected string $title = '';
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+}
