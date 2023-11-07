9e899337a884a627c202e24d83c41ef48fe162bb
Philipp Halle <p.halle@web-vision.de>
2023-11-07 10:41:33 +0100

    [BUGFIX] Fix phpstan errors in contract labels class

--- Classes/Tca/ContractLabels.php
+++ Classes/Tca/ContractLabels.php
@@ -9,16 +9,21 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class ContractLabels
 {
-    public function getTitle(&$parameters)
+    /**
+     * @param array{items: list<array<string, string|int>>} $parameters
+     */
+    public function getTitle(array &$parameters): void
     {
         $contractRepository = GeneralUtility::makeInstance(ContractRepository::class);
         $contract = $contractRepository->findByUid($parameters['row']['uid']);
-        $newTitle = '';
-        if ($contract->getProfile()) {
-            $newTitle .= $contract->getProfile()->getLastName() . ', ' . $contract->getProfile()->getFirstName();
-        }
-        if ($contract->getEmployeeType()) {
-            $newTitle .= ' (' . $contract->getEmployeeType()->getTitle() . ')';
+        if ($contract) {
+            $newTitle = '';
+            if ($contract->getProfile()) {
+                $newTitle .= $contract->getProfile()->getLastName() . ', ' . $contract->getProfile()->getFirstName();
+            }
+            if ($contract->getEmployeeType()) {
+                $newTitle .= ' (' . $contract->getEmployeeType()->getTitle() . ')';
+            }
         }
         $parameters['title'] = $newTitle;
     }
