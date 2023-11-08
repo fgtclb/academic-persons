0f8e1717dc55488b51c4b2411f301874dcbcf9b1
Tim Schreiner <tim.schreiner@km2.de>
2023-11-08 09:46:58 +0100

    [TASK] Add domain model for profile information

---
+++ Classes/Domain/Model/ProfileInformation.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "academic_persons" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Fgtclb\AcademicPersons\Domain\Model;
+
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+class ProfileInformation extends AbstractEntity
+{
+    protected string $type = '';
+
+    protected string $title = '';
+
+    protected string $bodytext = '';
+
+    protected string $link = '';
+
+    protected int $year = 0;
+
+    public function getType(): string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): void
+    {
+        $this->type = $type;
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getBodytext(): string
+    {
+        return $this->bodytext;
+    }
+
+    public function setBodytext(string $bodytext): void
+    {
+        $this->bodytext = $bodytext;
+    }
+
+    public function getLink(): string
+    {
+        return $this->link;
+    }
+
+    public function setLink(string $link): void
+    {
+        $this->link = $link;
+    }
+
+    public function getYear(): int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): void
+    {
+        $this->year = $year;
+    }
+}
