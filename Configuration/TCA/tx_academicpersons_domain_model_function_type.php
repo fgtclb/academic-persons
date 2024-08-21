3712e62522ab4baf86e40192b608d9f6ef0129a8
Philipp Halle <p.halle@web-vision.de>
2024-08-21 15:16:29 +0200

    [TASK] Add TCA configuration and icons for units and functions

---
+++ Configuration/TCA/tx_academicpersons_domain_model_function_type.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of the "academic_persons" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+$ll = fn (string $langKey): string => 'LLL:EXT:academic_persons/Resources/Private/Language/locallang_tca.xlf:tx_academicpersons_domain_model_function_type.' . $langKey;
+ 
+return [
+    'ctrl' => [
+        'label' => 'function_name',
+        'default_sortby' => 'function_name',
+        'crdate' => 'crdate',
+        'tstamp' => 'tstamp',
+        'title' => $ll('ctrl.label'),
+        'delete' => 'deleted',
+        'origUid' => 't3_origuid',
+        'transOrigPointerField' => 'l10n_parent',
+        'transOrigDiffSourceField' => 'l10n_diffsource',
+        'languageField' => 'sys_language_uid',
+        'translationSource' => 'l10n_source',
+        'enablecolumns' => [
+            'disabled' => 'hidden',
+        ],
+        'searchFields' => 'function_name',
+        'typeicon_classes' => [
+            'default' => 'tx_academicpersons_domain_model_function_type',
+        ],
+    ],
+    'columns' => [
+        'hidden' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.visible',
+            'config' => [
+                'type' => 'check',
+                'renderType' => 'checkboxToggle',
+                'items' => [
+                    [
+                        0 => '',
+                        'invertStateDisplay' => true,
+                    ],
+                ],
+            ],
+        ],
+        'sys_language_uid' => [
+            'exclude' => true,
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language',
+            'config' => [
+                'type' => 'language',
+            ],
+        ],
+        'l10n_parent' => [
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'label' => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.l18n_parent',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    [
+                        '',
+                        0,
+                    ],
+                ],
+                'foreign_table' => 'tx_academicpersons_domain_model_function_type',
+                'foreign_table_where' => 'AND {#tx_academicpersons_domain_model_function_type}.{#pid}=###CURRENT_PID### AND {#tx_academicpersons_domain_model_function_type}.{#sys_language_uid} IN (-1,0)',
+                'default' => 0,
+            ],
+        ],
+        'l10n_source' => [
+            'config' => [
+                'type' => 'passthrough',
+            ],
+        ],
+        'his_id' => [
+            'config' => [
+                'type' => 'passthrough',
+            ],
+        ],
+        'function_name' => [
+            'label' => $ll('columns.function_name.label'),
+            'config' => [
+                'type' => 'input',
+                'size' => 50,
+                'max' => 60,
+                'eval' => 'required',
+            ],
+        ],
+        'function_name_female' => [
+            'label' => $ll('columns.function_name_female.label'),
+            'config' => [
+                'type' => 'input',
+                'size' => 50,
+                'max' => 60,
+            ],
+        ],
+        'function_name_male' => [
+            'label' => $ll('columns.function_name_male.label'),
+            'config' => [
+                'type' => 'input',
+                'size' => 50,
+                'max' => 60,
+            ],
+        ],
+    ],
+    'palettes' => [
+        'language' => [
+            'showitem' => implode(',', [
+                'sys_language_uid;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:sys_language_uid_formlabel',
+                'l10n_parent',
+            ]),
+        ],
+    ],
+    'types' => [
+        '1' => [
+            'showitem' => implode(',', [
+                '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general',
+                    'function_name',
+                    'function_name_female',
+                    'function_name_male',
+                '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language',
+                    '--palette--;;language',
+                '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended',
+            ]),
+        ],
+    ],
+];
