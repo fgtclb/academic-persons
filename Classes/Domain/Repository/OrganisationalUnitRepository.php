3a1b086492f4afd3237e0269579fcaf9fb29d3e2
Philipp Halle <p.halle@web-vision.de>
2024-10-09 15:39:21 +0200

    [TASK] Add models and repositories for units and functions

---
+++ Classes/Domain/Repository/OrganisationalUnitRepository.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "academic_persons" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace Fgtclb\AcademicPersons\Domain\Repository;
+
+use Fgtclb\AcademicPersons\Domain\Model\OrganisationalUnit;
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+/**
+ * @extends Repository<OrganisationalUnit>
+ */
+class OrganisationalUnitRepository extends Repository
+{
+}
