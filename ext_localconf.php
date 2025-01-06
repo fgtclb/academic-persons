f2b852d781afdf4427963f1d6c6cce21c03544fc
Philipp Halle <p.halle@web-vision.de>
2025-01-06 13:44:52 +0100

    [TASK] Make handling of page.tsconfig compatible with v12 and v13

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,37 +10,54 @@ declare(strict_types=1);
  */
 
 use Fgtclb\AcademicPersons\Controller\ProfileController;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['profile'] = 'EXT:academic_persons/Configuration/CKEditor/Profile.yaml';
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['linkOnly'] = 'EXT:academic_persons/Configuration/CKEditor/LinkOnly.yaml';
+defined('TYPO3') or die;
 
-ExtensionUtility::configurePlugin(
-    'AcademicPersons',
-    'List',
-    [
-        ProfileController::class => 'list',
-    ]
-);
+(static function (): void {
+    $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
 
-ExtensionUtility::configurePlugin(
-    'AcademicPersons',
-    'Detail',
-    [
-        ProfileController::class => 'detail',
-    ]
-);
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['profile'] = 'EXT:academic_persons/Configuration/CKEditor/Profile.yaml';
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['linkOnly'] = 'EXT:academic_persons/Configuration/CKEditor/LinkOnly.yaml';
 
-ExtensionUtility::configurePlugin(
-    'AcademicPersons',
-    'ListAndDetail',
-    [
-        ProfileController::class => 'list,detail',
-    ]
-);
+    ExtensionUtility::configurePlugin(
+        'AcademicPersons',
+        'List',
+        [
+            ProfileController::class => 'list',
+        ]
+    );
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['academicPersons']
-    = \Fgtclb\AcademicPersons\Hook\DataHandlerHooks::class;
+    ExtensionUtility::configurePlugin(
+        'AcademicPersons',
+        'Detail',
+        [
+            ProfileController::class => 'detail',
+        ]
+    );
 
-ExtensionManagementUtility::addPageTSConfig('@import \'EXT:academic_persons/Configuration/TSconfig/page.tsconfig\'');
+    ExtensionUtility::configurePlugin(
+        'AcademicPersons',
+        'ListAndDetail',
+        [
+            ProfileController::class => implode(',', [
+                'list',
+                'detail',
+            ]),
+        ]
+    );
+
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['academicPersons']
+        = \Fgtclb\AcademicPersons\Hook\DataHandlerHooks::class;
+
+    // Starting with TYPO3 v12.0 Configuration/page.tsconfig in an Extension is automatically loaded during build time.
+    // @see https://docs.typo3.org/m/typo3/reference-tsconfig/12.4/en-us/UsingSetting/PageTSconfig.html#pagesettingdefaultpagetsconfig
+    if ($versionInformation->getMajorVersion() < 12) {
+        ExtensionManagementUtility::addPageTSConfig('
+            @import \'EXT:academic_programs/Configuration/page.tsconfig\'
+        ');
+    }
+})();
