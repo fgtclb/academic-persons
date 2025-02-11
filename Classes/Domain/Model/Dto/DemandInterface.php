30d5b9191f4bee0421570f3e19a351533db7f1aa
Stefan Bürk <stefan@buerk.tech>
2025-02-11 21:21:29 +0100

    [TASK] Add storage pages and plugin fallback option to demand object
    This change adds storage pages and plugin fallback enforce
    option to the `ProfileDemand` implementation and commented
    out to the `DemandInterface` with `@todo's` to make add'em
    breaking with next major version.
    
    `ProfileController::adoptSettings()` will adopt options to
    the DemandObject if method exits, otherwise still setting
    options directly to the `ProfileRepository` but triggering
    deprecation now.
    
    Classes implemented based on `DemandInterface` will trigger
    E_USER_DEPRECATED passed to ProfileController::listAction()
    or `ProfileRepository::findByDemand()` when follwing methods
    are missing:
    
    * DemandInterface::setStoragePages(string): self
    * DemandInterface::getStoragePages(): string
    * DemandInterface::setFallbackForNonTranslated(int): self
    * DemandInterface::getFallbackForNonTranslated(): int
    
    Default implementation `ProfileDemand` implements aforementioned
    methods, matching commented method signatures of the interface.

--- Classes/Domain/Model/Dto/DemandInterface.php
+++ Classes/Domain/Model/Dto/DemandInterface.php
@@ -13,27 +13,93 @@ namespace Fgtclb\AcademicPersons\Domain\Model\Dto;
 
 interface DemandInterface
 {
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function getGroupBy(): string;
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function setGroupBy(string $groupBy): self;
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function getSortBy(): string;
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function setSortBy(string $sortBy): self;
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function getSortByDirection(): string;
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function setSortByDirection(string $sortByDirection): self;
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function setCurrentPage(int $currentPage): self;
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function getCurrentPage(): int;
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function setAlphabetFilter(string $alphabetFilter): self;
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function getAlphabetFilter(): string;
 
     public function setProfileList(string $profileList): self;
 
     public function getProfileList(): string;
+
+    /**
+     * Not usable for hydration or direct extbase request argument mapping,
+     * only to transport direct storage page selection within the DTO to the
+     * {@see ProfileRepository::findByDemand()} method.
+     *
+     * @todo Add to interface with next major version as breaking change.
+     */
+    //public function getStoragePages(): string;
+
+    /**
+     * Not usable for hydration or direct extbase request argument mapping,
+     * only to transport direct storage page selection within the DTO to the
+     * {@see ProfileRepository::findByDemand()} method.
+     *
+     * @todo Add to interface with next major version as breaking change.
+     */
+    //public function setStoragePages(string $storagePages): ProfileDemand;
+
+    /**
+     * Not usable for hydration or direct extbase request argument mapping,
+     * only to transport direct storage page selection within the DTO to the
+     * {@see ProfileRepository::findByDemand()} method.
+     *
+     * @todo Add to interface with next major version as breaking change.
+     */
+    // public function getFallbackForNonTranslated(): int;
+
+    /**
+     * Not usable for hydration or direct extbase request argument mapping,
+     * only to transport direct storage page selection within the DTO to the
+     * {@see ProfileRepository::findByDemand()} method.
+     *
+     * @todo Add to interface with next major version as breaking change.
+     */
+    //public function setFallbackForNonTranslated(int $fallbackForNonTranslated): ProfileDemand;
 }
--- Classes/Domain/Model/Dto/ProfileDemand.php
+++ Classes/Domain/Model/Dto/ProfileDemand.php
@@ -11,83 +11,168 @@ declare(strict_types=1);
 
 namespace Fgtclb\AcademicPersons\Domain\Model\Dto;
 
+use Fgtclb\AcademicPersons\Domain\Repository\ProfileRepository;
+
 class ProfileDemand implements DemandInterface
 {
     protected string $groupBy = '';
-
     protected string $sortBy = 'lastName';
-
     protected string $sortByDirection = 'asc';
-
     protected int $currentPage = 1;
-
     protected string $alphabetFilter = '';
-
     protected string $profileList = '';
+    private string $storagePages = '';
+    private int $fallbackForNonTranslated = 0;
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function getGroupBy(): string
     {
         return $this->groupBy;
     }
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function setGroupBy(string $groupBy): self
     {
         $this->groupBy = $groupBy;
         return $this;
     }
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function getSortBy(): string
     {
         return $this->sortBy;
     }
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function setSortBy(string $sortBy): self
     {
         $this->sortBy = $sortBy;
         return $this;
     }
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function getSortByDirection(): string
     {
         return $this->sortByDirection;
     }
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function setSortByDirection(string $sortByDirection): self
     {
         $this->sortByDirection = $sortByDirection;
         return $this;
     }
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function getCurrentPage(): int
     {
         return $this->currentPage;
     }
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function setCurrentPage(int $currentPage): self
     {
         $this->currentPage = $currentPage;
         return $this;
     }
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function getAlphabetFilter(): string
     {
         return $this->alphabetFilter;
     }
 
+    /**
+     * Does not have any effect when {@see self::getProfileList()} is not empty.
+     */
     public function setAlphabetFilter(string $alphabetFilter): self
     {
         $this->alphabetFilter = $alphabetFilter;
         return $this;
     }
 
+    /**
+     * Overrules all other filter options when not empty string,
+     * except special settings:
+     *
+     * - {@see self::getFallbackForNonTranslated()}
+     * - {@see self::getStoragePages()}
+     */
     public function getProfileList(): string
     {
         return $this->profileList;
     }
 
+    /**
+     * Overrules all other filter options when not empty string,
+     * except special settings:
+     *
+     * - {@see self::getFallbackForNonTranslated()}
+     * - {@see self::getStoragePages()}
+     */
     public function setProfileList(string $profileList): self
     {
         $this->profileList = $profileList;
+        return $this;
+    }
+
+    /**
+     * Not usable for hydration or direct extbase request argument mapping,
+     * only to transport direct storage page selection within the DTO to the
+     * {@see ProfileRepository::findByDemand()} method.
+     */
+    public function getStoragePages(): string
+    {
+        return $this->storagePages;
+    }
+
+    /**
+     * Not usable for hydration or direct extbase request argument mapping,
+     * only to transport direct storage page selection within the DTO to the
+     * {@see ProfileRepository::findByDemand()} method.
+     */
+    public function setStoragePages(string $storagePages): ProfileDemand
+    {
+        $this->storagePages = $storagePages;
+        return $this;
+    }
+
+    /**
+     * Not usable for hydration or direct extbase request argument mapping,
+     * only to transport direct storage page selection within the DTO to the
+     * {@see ProfileRepository::findByDemand()} method.
+     */
+    public function getFallbackForNonTranslated(): int
+    {
+        return $this->fallbackForNonTranslated;
+    }
+
+    /**
+     * Not usable for hydration or direct extbase request argument mapping,
+     * only to transport direct storage page selection within the DTO to the
+     * {@see ProfileRepository::findByDemand()} method.
+     */
+    public function setFallbackForNonTranslated(int $fallbackForNonTranslated): ProfileDemand
+    {
+        $this->fallbackForNonTranslated = $fallbackForNonTranslated;
         return $this;
     }
 }
