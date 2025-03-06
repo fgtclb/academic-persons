3246906d2a51db3df83c6221b065271ac1262a36
Philipp Halle <p.halle@web-vision.de>
2025-03-06 16:13:39 +0100

    [TASK] Add functional tests for selected profiles and contracts plugins
    Two new plugins has been introduced recently [1]
    without some test coverage for the new plugins.
    
    Both of them are sensitive to the same technical
    issue regarding site language configuration and
    fallback mode like the selected profiles for the
    `listAndDetail` and `list` plugin, which had been
    fixed recently and covered by tests. [2][3]
    
    During the review the sensitive part has been
    directly addressed and are now covered with
    equaliant tests to cover the new plugins in
    the same way.
    
    [1] https://github.com/fgtclb/academic-persons/pull/53
    [2] https://github.com/fgtclb/academic-persons/commit/7603c841584340b7db7f2946a957cdfdb5ee89a3
    [3] https://github.com/fgtclb/academic-persons/commit/bfa8179f7e2c6e04975a244a85441fc2ef2ed421

---
+++ Tests/Functional/Plugins/AcademicPersonsSelectedContractsPluginTest.php
@@ -0,0 +1,228 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Fgtclb\AcademicPersons\Tests\Functional\Plugins;
+
+use Fgtclb\AcademicPersons\Tests\Functional\Fixtures\Trait\SiteBasedTestTrait;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequest;
+use TYPO3\TestingFramework\Core\Functional\Framework\Frontend\InternalRequestContext;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+
+final class AcademicPersonsSelectedContractsPluginTest extends FunctionalTestCase
+{
+    use SiteBasedTestTrait;
+
+    protected array $coreExtensionsToLoad = [
+        'typo3/cms-fluid-styled-content',
+    ];
+
+    protected array $testExtensionsToLoad = [
+        'georgringer/numbered-pagination',
+        'fgtclb/academic-persons',
+        'tests/plugin-templates',
+    ];
+
+    protected array $configurationToUseInTestInstance = [
+        'SYS' => [
+            'encryptionKey' => '4408d27a916d51e624b69af3554f516dbab61037a9f7b9fd6f81b4d3bedeccb6',
+            'features' => [
+                'subrequestPageErrors' => true,
+            ],
+        ],
+        'FE' => [
+            'cacheHash' => [
+                'requireCacheHashPresenceParameters' => ['value', 'testing[value]', 'tx_testing_link[value]'],
+                'excludedParameters' => ['L', 'tx_testing_link[excludedValue]'],
+                'enforceValidation' => true,
+            ],
+            'debug' => false,
+        ],
+        'SC_OPTIONS' => [
+            'Core/TypoScript/TemplateService' => [
+                'runThroughTemplatesPostProcessing' => [
+                    'FunctionalTest' => \TYPO3\TestingFramework\Core\Functional\Framework\Frontend\Hook\TypoScriptInstructionModifier::class . '->apply',
+                ],
+            ],
+        ],
+    ];
+
+    protected const LANGUAGE_PRESETS = [
+        'EN' => ['id' => 0, 'title' => 'English', 'locale' => 'en_US.UTF8', 'iso' => 'en', 'hrefLang' => 'en-US', 'direction' => ''],
+        'DE' => ['id' => 1, 'title' => 'Deutsch', 'locale' => 'de_DE.UTF8', 'iso' => 'de', 'hrefLang' => 'de-DE', 'direction' => ''],
+        'FR' => ['id' => 2, 'title' => 'French', 'locale' => 'fr_FR.UTF8', 'iso' => 'fr', 'hrefLang' => 'fr-FR', 'direction' => ''],
+    ];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    protected function tearDown(): void
+    {
+        GeneralUtility::rmdir($this->instancePath . '/typo3conf/sites', true);
+        parent::tearDown();
+    }
+
+    private function setUpFrontendRootPageForTestCase(): void
+    {
+        $this->setUpFrontendRootPage(
+            1,
+            [
+                'constants' => [
+                    'EXT:fluid_styled_content/Configuration/TypoScript/constants.typoscript',
+                    'EXT:academic_persons/Configuration/TypoScript/constants.typoscript',
+                    'EXT:test_plugin_templates/Configuration/TypoScript/constants.typoscript',
+                    'EXT:academic_persons/Tests/Functional/Plugins/Fixtures/TypoScript/Constants/PluginConfiguration.typoscript',
+                ],
+                'setup' => [
+                    'EXT:fluid_styled_content/Configuration/TypoScript/setup.typoscript',
+                    'EXT:academic_persons/Configuration/TypoScript/setup.typoscript',
+                    'EXT:test_plugin_templates/Configuration/TypoScript/setup.typoscript',
+                    'EXT:academic_persons/Tests/Functional/Plugins/Fixtures/TypoScript/Setup/Rendering.typoscript',
+                ],
+            ]
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function defaultLanguageOnly_allContractsSelected(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/AcademicPersonsSelectedContractsPlugin/defaultLanguageOnly_allContractsSelected.csv');
+        $this->setUpFrontendRootPageForTestCase();
+        $this->writeSiteConfiguration(
+            'acme',
+            $this->buildSiteConfiguration(1, 'https://www.acme.com/'),
+            [
+                $this->buildDefaultLanguageConfiguration('EN', '/'),
+            ]
+        );
+
+        $requestContext = new InternalRequestContext();
+        $request = new InternalRequest('https://www.acme.com/home');
+        $response = $this->executeFrontendSubRequest($request, $requestContext);
+        static::assertSame(200, $response->getStatusCode());
+
+        $content = (string)$response->getBody();
+        static::assertStringContainsString('<h2>Selected Contracts</h2>', $content);
+        static::assertStringContainsString('#0(2): Manager', $content);
+        static::assertStringContainsString('#1(1): Worker', $content);
+    }
+
+    /**
+     * @test
+     */
+    public function defaultLanguageOnly_oneContractSelected(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/AcademicPersonsSelectedContractsPlugin/defaultLanguageOnly_oneContractSelected.csv');
+        $this->setUpFrontendRootPageForTestCase();
+        $this->writeSiteConfiguration(
+            'acme',
+            $this->buildSiteConfiguration(1, 'https://www.acme.com/'),
+            [
+                $this->buildDefaultLanguageConfiguration('EN', '/'),
+            ]
+        );
+
+        $requestContext = new InternalRequestContext();
+        $request = new InternalRequest('https://www.acme.com/home');
+        $response = $this->executeFrontendSubRequest($request, $requestContext);
+        static::assertSame(200, $response->getStatusCode());
+
+        $content = (string)$response->getBody();
+        static::assertStringContainsString('<h2>Selected Contracts</h2>', $content);
+        static::assertStringContainsString('#0(2): Manager', $content);
+        static::assertStringNotContainsString('Worker', $content);
+    }
+
+    /**
+     * @test
+     */
+    public function fullyLocalized_allContractsSelected_allContractsLocalized(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/AcademicPersonsSelectedContractsPlugin/fullyLocalized_allContractsSelected_allContractsLocalized.csv');
+        $this->setUpFrontendRootPageForTestCase();
+        $this->writeSiteConfiguration(
+            'acme',
+            $this->buildSiteConfiguration(1, 'https://www.acme.com/'),
+            [
+                $this->buildDefaultLanguageConfiguration('EN', '/'),
+                //$this->buildLanguageConfiguration('DE', '/de/'),
+                $this->buildLanguageConfiguration('DE', '/de/', ['EN'], 'content_fallback'),
+            ]
+        );
+
+        $requestContext = new InternalRequestContext();
+        $request = new InternalRequest('https://www.acme.com/de/home');
+        $response = $this->executeFrontendSubRequest($request, $requestContext);
+        static::assertSame(200, $response->getStatusCode());
+
+        $content = (string)$response->getBody();
+        static::assertStringContainsString('<h2>Selected Contracts</h2>', $content);
+        static::assertStringContainsString('#0(3): [DE] Manager', $content);
+        static::assertStringContainsString('#1(1): [DE] Arbeiter', $content);
+        static::assertStringNotContainsString('[EN] Manager', $content);
+        static::assertStringNotContainsString('[EN] Worker', $content);
+    }
+
+    /**
+     * @test
+     */
+    public function fullyLocalized_allContractsSelected_notAllContractsLocalized_strictMode(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/AcademicPersonsSelectedContractsPlugin/fullyLocalized_allContractsSelected_notAllContractsLocalized.csv');
+        $this->setUpFrontendRootPageForTestCase();
+        $this->writeSiteConfiguration(
+            'acme',
+            $this->buildSiteConfiguration(1, 'https://www.acme.com/'),
+            [
+                $this->buildDefaultLanguageConfiguration('EN', '/'),
+                $this->buildLanguageConfiguration('DE', '/de/', [], 'strict'),
+            ]
+        );
+
+        $requestContext = new InternalRequestContext();
+        $request = new InternalRequest('https://www.acme.com/de/home');
+        $response = $this->executeFrontendSubRequest($request, $requestContext);
+        static::assertSame(200, $response->getStatusCode());
+
+        $content = (string)$response->getBody();
+        static::assertStringContainsString('<h2>Selected Contracts</h2>', $content);
+        static::assertStringContainsString('#0(3): [EN] Manager', $content);
+        static::assertStringContainsString('#1(1): [DE] Arbeiter', $content);
+        static::assertStringNotContainsString('[DE] Manager', $content);
+        static::assertStringNotContainsString('[EN] Worker', $content);
+    }
+
+    /**
+     * @test
+     */
+    public function fullyLocalized_SelectedContracts_notAllContractsLocalized_fallbackMode(): void
+    {
+        $this->importCSVDataSet(__DIR__ . '/Fixtures/AcademicPersonsSelectedContractsPlugin/fullyLocalized_allContractsSelected_notAllContractsLocalized.csv');
+        $this->setUpFrontendRootPageForTestCase();
+        $this->writeSiteConfiguration(
+            'acme',
+            $this->buildSiteConfiguration(1, 'https://www.acme.com/'),
+            [
+                $this->buildDefaultLanguageConfiguration('EN', '/'),
+                $this->buildLanguageConfiguration('DE', '/de/', ['EN'], 'content_fallback'),
+            ]
+        );
+
+        $requestContext = new InternalRequestContext();
+        $request = new InternalRequest('https://www.acme.com/de/home');
+        $response = $this->executeFrontendSubRequest($request, $requestContext);
+        static::assertSame(200, $response->getStatusCode());
+
+        $content = (string)$response->getBody();
+        static::assertStringContainsString('<h2>Selected Contracts</h2>', $content);
+        static::assertStringContainsString('#0(3): [EN] Manager', $content);
+        static::assertStringContainsString('#1(1): [DE] Arbeiter', $content);
+        static::assertStringNotContainsString('[DE] Manager', $content);
+        static::assertStringNotContainsString('[EN] Worker', $content);
+    }
+}
