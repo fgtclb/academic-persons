91ee3955214f4e82615ae3917b05764f201abb47
Stefan Bürk <stefan@buerk.tech>
2025-03-11 12:44:39 +0100

    [TASK] Solve PHPStan issues in backported code
    Backporting changes from the compatibility branch,
    stating to have v11, v12 and v13 support, added
    the one or other phpstan issue. Mainly, because
    the support for v11 and v12 has been partly broken
    and not correctly handled - or adjusted in phpstan
    configuration.
    
    This change cleans these up.

--- Classes/Domain/Repository/ContractRepository.php
+++ Classes/Domain/Repository/ContractRepository.php
@@ -32,7 +32,9 @@ class ContractRepository extends Repository
         // Selected uid's are default language and we need to configure extbase in away to
         // properly handle the overlay. This is adopted from the generic extbase backend
         // implementation.
-        if ((new Typo3Version())->getMajorVersion() >= 12) {
+        if (method_exists($query->getQuerySettings(), 'getLanguageAspect')
+            && method_exists($query->getQuerySettings(), 'setLanguageAspect')
+        ) {
             $currentLanguageAspect = $query->getQuerySettings()->getLanguageAspect();
             $changedLanguageAspect = new LanguageAspect(
                 $currentLanguageAspect->getId(),
@@ -42,7 +44,9 @@ class ContractRepository extends Repository
             $query->getQuerySettings()->setLanguageAspect($changedLanguageAspect);
         } else {
             // @todo Remove this when TYPO3 v11 support is dropped with 2.x.x.
-            $query->getQuerySettings()->setLanguageOverlayMode(true);
+            if (method_exists($query->getQuerySettings(), 'setLanguageOverlayMode')) {
+                $query->getQuerySettings()->setLanguageOverlayMode(true);
+            }
         }
         $query->getQuerySettings()->setRespectStoragePage(false);
         $query->getQuerySettings()->setRespectSysLanguage(false);
