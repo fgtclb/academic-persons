b630b8f0b37e85dbd21c6bbefb80fda299f41261
Philipp Halle <p.halle@web-vision.de>
2025-03-24 08:20:45 +0100

    [!!!][FEATURE] Introduce new category type handlinging
    The old TYPO3 Enumaration based category handling system is now
    replaced with a new category handling type providing the ability
    to define extension based category types using a yaml-file based
    approach.
    
    `Configuration/CategoryTypes.yaml` files from all TYPO3 extensions
    are automatically parsed and registered, needing to provide types
    under the `types` key word using following syntax:
    
    ```yaml
    types:
      - identifier: <unique_identifier>
        title: '<type-translation-lable-using-full-LLL-syntax'
        group: <group-identifier>
        icon: '<icon-file-using-EXT-syntax-needs-to-be-an-as-svg>'
    ```
    
    The `group-identifier` is important in the way that this is
    used to group category types for a specific extension, and
    academic extensions supporting (requiring) the category-type
    handling based on `EXT:category_types` uses a selected group
    identifier. Currently extension group identifiers are:
    
    - `EXT:academic_partners`: partners
    - `EXT:academic_programs`: programs
    - `EXT:academic_projects`: projects
    
    Aforementioned extensions also provides default category types
    for the mentioned group, which can be extended in projects by
    providing `Configuration/CategoryTypes.yaml` file(s) within a
    sitepackage extension or multiple local path extensions.
    
    Extensions are allowed to register additional category-types
    for aforementioned groups by simply using the identifier for
    the `group` key.
    
    The new category-type handling API is considered experimental
    for now and may still change in early 2.x.x versions, until a
    stable implementation regarding signature has been etablished.
    
    In general projects may implement own category-type handling
    based on `EXT:category_types`, but needs to adopt implementation
    by looking up the aforementioned extensions. A comprehensive
    documentation for developers will be added in a later point.
    
    The same counts for upgrade strategy from old 1.x.x enumaration
    based category-type handling to the new 2.x.x implementation.
    Bare with us, as we are still testing the new api and needed
    to add the new handling now to test it in real world projects.
    
    With the new handling, stricter PHP typeing has been added and
    allows us to get rid of a lot of PHPStan ignore patterns which
    are removed with the same change, working towards a more higher
    code quality standard throughout all academic extensions.
    
    Used command(s):
    
    ```shell
    cat <<< $(
      jq '."autoload-dev"."psr-4"."FGTCLB\\CategoryTypes\\Tests\\" = "Tests/"' \
        packages/fgtclb/typo3-category-types/composer.json
    ) > packages/fgtclb/typo3-category-types/composer.json \
    && Build/Scripts/runTests.sh -t 12 -p 8.1 -s composerUpdate \
    && Build/Scripts/runTests.sh -t 12 -p 8.1 -s cgl \
    && Build/Scripts/runTests.sh -t 12 -p 8.1 -s phpstanGenerateBaseline \
    && Build/Scripts/runTests.sh -t 13 -p 8.2 -s composerUpdate \
    && Build/Scripts/runTests.sh -t 13 -p 8.2 -s phpstanGenerateBaseline
    ```
    
    Co-authored-by: Stefan Bürk <stefan@buerk.tech>

--- ext_localconf.php
+++ ext_localconf.php
@@ -10,15 +10,11 @@ declare(strict_types=1);
  */
 
 use Fgtclb\AcademicPersons\Controller\ProfileController;
-use TYPO3\CMS\Core\Information\Typo3Version;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') or die;
 
 (static function (): void {
-    $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
 
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['profile'] = 'EXT:academic_persons/Configuration/CKEditor/Profile.yaml';
     $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['linkOnly'] = 'EXT:academic_persons/Configuration/CKEditor/LinkOnly.yaml';
@@ -88,17 +84,4 @@ defined('TYPO3') or die;
 
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['academicPersons']
         = \Fgtclb\AcademicPersons\Hook\DataHandlerHooks::class;
-
-    if ($versionInformation->getMajorVersion() < 12) {
-        // Starting with TYPO3 v12.0 Configuration/page.tsconfig in an Extension is automatically loaded during build time.
-        // @see https://docs.typo3.org/m/typo3/reference-tsconfig/12.4/en-us/UsingSetting/PageTSconfig.html#pagesettingdefaultpagetsconfig
-        ExtensionManagementUtility::addPageTSConfig('
-            @import \'EXT:academic_programs/Configuration/page.tsconfig\'
-        ');
-
-        // Starting with TYPO3 v12.0 can this remove
-        // @see https://docs.typo3.org/m/typo3/reference-coreapi/12.4/en-us/ExtensionArchitecture/HowTo/UpdateExtensions/UpdateWizards/Creation.html#upgrade-wizard-interface
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['academicPerson_pluginContent']
-            = \Fgtclb\AcademicPersons\Upgrades\PluginContentUpgrade::class;
-    }
 })();
