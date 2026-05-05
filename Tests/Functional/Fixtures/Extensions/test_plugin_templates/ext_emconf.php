c96a4128800b0d5b8ae78e6da90115482eb2c239
Stefan Bürk <stefan@buerk.tech>
2026-05-05 16:48:41 +0200

    [TASK] ACE-228: Normalize `ext_emconf.php`
    This changes uses the `ext_emconf.php` normalization of the
    upcoming tool `packwright` to all of the split- and test
    extension files in a good shape.
    
    Used command(s):
    
    ```bash
    pkw-dev extemconf:normalize -t 12 -- packages/**/ext_emconf.php && \
    pkw-dev extemconf:set \
      --ext-version=2.3.2 \
      --author-company='FGTCLB GmbH' \
      --author-email='hello@fgtclb.com' \
      -- packages/**/ext_emconf.php
    ```
    
    > [!NOTE]
    > `pkw (packwraight)` not realease in public yet. It's the upcoming
    > TYPO3 extension release took with a couple of helpers. Currently
    > only `ext_emconf.php` validation, normalization, value setting and
    > constraints handling is availalbe and the reason that the development
    > version has been used.
    
    This is done as preparation for the next release.

--- Tests/Functional/Fixtures/Extensions/test_plugin_templates/ext_emconf.php
+++ Tests/Functional/Fixtures/Extensions/test_plugin_templates/ext_emconf.php
@@ -3,21 +3,16 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'TESTS: Academic Persons Plugin Templates',
     'description' => 'Provide plugin template overrides for academic-persons for functional tests',
-    'category' => 'plugin',
-    'author' => 'Stefan Bürk',
-    'author_email' => 'stefan@buerk.tech',
-    'author_company' => 'web-vision GmbH',
-    'state' => 'beta',
     'version' => '2.3.2',
-    'clearCacheOnLoad' => true,
+    'category' => 'plugin',
+    'state' => 'beta',
+    'author' => 'Stefan Bürk',
+    'author_email' => 'hello@fgtclb.com',
+    'author_company' => 'FGTCLB GmbH',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.22-13.4.99',
             'academic_persons' => '2.3.2',
-        ],
-        'conflicts' => [
-        ],
-        'suggests' => [
         ],
     ],
 ];
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,10 +1,14 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'author' => 'FGTCLB',
-    'author_company' => 'FGTCLB GmbH',
-    'author_email' => 'hello@fgtclb.com',
+    'title' => 'FGTCLB: Academic Persons',
+    'description' => 'Adds a person database to TYPO3 with plugins to show them in the frontend.',
+    'version' => '2.3.2',
     'category' => 'plugin',
+    'state' => 'beta',
+    'author' => 'FGTCLB',
+    'author_email' => 'hello@fgtclb.com',
+    'author_company' => 'FGTCLB GmbH',
     'constraints' => [
         'depends' => [
             'typo3' => '12.4.22-13.4.99',
@@ -15,14 +19,8 @@ $EM_CONF[$_EXTKEY] = [
             'rte_ckeditor' => '12.4.22-13.4.99',
             'academic_base' => '2.3.2',
         ],
-        'conflicts' => [
-        ],
         'suggests' => [
             'numbered_pagination' => '2.1.0-2.99.99',
         ],
     ],
-    'description' => 'Adds a person database to TYPO3 with plugins to show them in the frontend.',
-    'state' => 'beta',
-    'title' => 'FGTCLB: Academic Persons',
-    'version' => '2.3.2',
 ];
