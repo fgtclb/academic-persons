fgtclb/academic-extensions

[!!!][FEATURE] ACE-503: Model settings as sections
The settings file of academic_persons could say which field is
required or locked and nothing else: a `profileInformationsTypes` map
naming the seven timeline entry types, and a `validations` map with
one flag list per record type. The editing frontend this chain
replaces needs the order of the fields, the control each is rendered
with, its help text, the rows and actions of a sortable list and the
character limit of a rich text field. `Settings.yaml` now declares
all of that in four maps - `profile`, `special`, `contracts` and
`documentSections` - and `AcademicPersonsSettingsFactory` builds a
typed graph from them: ProfileSection and ProfileField, SpecialField,
ContractField, ContractContactSection and ContractContactField,
DocumentSection, and PublicProfileSettings for the detail layout that
shares the `profile` map with the editable fields. Every set stays
with its section; nothing falls back from one to another.

The factory is glue on the shared classes of academic_base: the
SettingsFileLoader does the package walk and the cache round trip,
and every flag list goes through ValidationNormalizer, which learns
`url`, `tel`, `textarea` and `html`, takes the column name, the render
type the flags start from and a character limit, and keeps the
normalised flag list on Validation next to `characterLimit` and
`isRichText()`. Deriving a TCA `type` from the settings' `fieldType`,
as the source branch did, was rejected: the TCA files own their
column types and the settings overlay editability and the required
state - only `email` and `number` set a type, as before. The
`minitems` of a required field is kept unconditional for the same
reason of not changing what the flat sets produced.

The backend consumes the graph. Five TCA files merge the set of their
own section through TcaValidationMerger; the profile information
table gets `types.<type>.columnsOverrides` instead, because its seven
timeline types share one table and 

packages/fgtclb/academic-persons/Classes/Settings/DocumentSection.php
@@ -1,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FGTCLB\AcademicPersons\Settings;
+
+use FGTCLB\AcademicBase\Settings\ValidationSet;
+use Symfony\Component\DependencyInjection\Attribute\Exclude;
+
+/**
+ * One list of records attached to a profile: the seven profile information
+ * types, and the contracts. `type` is the record type the rows are selected
+ * by and `fieldName` the profile relation they hang off. `rowFields` and
+ * `actions` are the ordered, validated lists of what a compact row shows and
+ * offers; `readOnly` reduces the actions to viewing regardless of the list.
+ * `helptexts` maps the field keys of the section's editor - the keys of its
+ * `validators` map, aliases included - to a label reference or text.
+ *
+ * @internal not part of public API.
+ */
+#[Exclude]
+final class DocumentSection
+{
+    public const SUPPORTED_ROW_FIELDS = ['from', 'to', 'date', 'title', 'description', 'position'];
+    public const SUPPORTED_CONTRACT_ROW_FIELDS = ['from', 'to', 'position'];
+    public const SUPPORTED_PROFILE_INFORMATION_ROW_FIELDS = ['from', 'to', 'date', 'title', 'description'];
+    public const SUPPORTED_ACTIONS = ['view', 'down', 'up', 'delete', 'edit'];
+
+    /**
+     * @param list<string> $rowFields
+     * @param list<string> $actions
+     * @param array<string, string> $helptexts Per field key of the section's editor, the label reference or text
+     */
+    public function __construct(
+        public readonly string $identifier,
+        public readonly string $label,
+        public readonly string $type,
+        public readonly string $fieldName,
+        public readonly bool $readOnly,
+        public readonly ValidationSet $validationSet,
+        public readonly int $position,
+        public readonly array $rowFields = [],
+        public readonly array $actions = [],
+        public readonly array $helptexts = [],
+    ) {}
+
+    /**
+     * @param array{
+     *     identifier: string,
+     *     label: string,
+     *     type: string,
+     *     fieldName: string,
+     *     readOnly: bool,
+     *     validationSet: ValidationSet,
+     *     position: int,
+     *     rowFields?: list<string>,
+     *     actions?: list<string>,
+     *     helptexts?: array<string, string>,
+     * } $array
+     */
+    public static function __set_state(array $array): self
+    {
+        return new self(
+            identifier: $array['identifier'],
+            label: $array['label'],
+            type: $array['type'],
+            fieldName: $array['fieldName'],
+            readOnly: $array['readOnly'],
+            validationSet: $array['validationSet'],
+            position: $array['position'],
+            rowFields: $array['rowFields'] ?? [],
+            actions: $array['actions'] ?? [],
+            helptexts: $array['helptexts'] ?? [],
+        );
+    }
+
+    public function isValid(): bool
+    {
+        return $this->identifier !== ''
+            && $this->label !== ''
+            && $this->type !== ''
+            && $this->fieldName !== '';
+    }
+
+    public function isContractSection(): bool
+    {
+        return $this->identifier === 'contracts'
+            || in_array($this->type, ['contract', 'contracts'], true);
+    }
+
+    public function allowsAction(string $action): bool
+    {
+        $normalizedAction = strtolower(trim($action));
+        return in_array($normalizedAction, $this->actions, true)
+            && (!$this->readOnly || $normalizedAction === 'view');
+    }
+
+    /**
+     * @return list<string>
+     */
+    public function getAllowedActions(): array
+    {
+        return array_values(array_filter(
+            $this->actions,
+            fn(string $action): bool => $this->allowsAction($action),
+        ));
+    }
+
+    public function allowsCreate(): bool
+    {
+        return !$this->readOnly;
+    }
+
+    public function allowsDragSorting(): bool
+    {
+        return $this->allowsAction('up') && $this->allowsAction('down');
+    }
+}

packages/fgtclb/academic-persons/Tests/Functional/Tca/EditSettingsIsolationTest.php
@@ -1,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FGTCLB\AcademicPersons\Tests\Functional\Tca;
+
+use FGTCLB\AcademicPersons\Tests\Functional\AbstractAcademicPersonsTestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+/**
+ * The section graph of `Settings.yaml` reaches the backend: every TCA file of
+ * this extension merges the set of its own section, so the flags an editor
+ * sees in the frontend are the flags the backend record editor enforces. The
+ * editing extension is deliberately not loaded here - the backend half applies
+ * without it.
+ */
+final class EditSettingsIsolationTest extends AbstractAcademicPersonsTestCase
+{
+    /**
+     * A document section addresses the records of its type only, through
+     * `columnsOverrides`, and the native date configuration of the columns
+     * stays untouched by it.
+     */
+    #[Test]
+    public function centralDocumentValidationIsAppliedToBackendTca(): void
+    {
+        $table = $GLOBALS['TCA']['tx_academicpersons_domain_model_profile_information'];
+
+        $this->assertArrayHasKey('columnsOverrides', $table['types']['cooperation']);
+        $overrides = $table['types']['cooperation']['columnsOverrides'];
+        $this->assertTrue($overrides['title']['config']['required']);
+        $this->assertTrue($overrides['date']['config']['required']);
+        $this->assertFalse($overrides['date_start']['config']['required']);
+        $this->assertFalse($overrides['date_end']['config']['required']);
+        $this->assertArrayNotHasKey('required', $table['columns']['date']['config']);
+        $this->assertSame('datetime', $table['columns']['date']['config']['type']);
+        $this->assertSame('date', $table['columns']['date']['config']['format']);
+        $this->assertSame('date', $table['columns']['date']['config']['dbType']);
+        $this->assertTrue($table['columns']['date']['config']['nullable']);
+    }
+
+    /**
+     * The profile table merges every profile section plus the direct special
+     * fields; the contact tables merge their own contact section each. The
+     * name fields are locked (`readonly`, `disabled`) and therefore not
+     * required, whatever else their flag list says.
+     */
+    #[Test]
+    public function centralProfileAndContactValidationIsAppliedToBackendTca(): void
+    {
+        $profile = $GLOBALS['TCA']['tx_academicpersons_domain_model_profile']['columns'];
+        $this->assertTrue($profile['gender']['config']['required']);
+        $this->assertSame(1, $profile['gender']['config']['minitems']);
+        $this->assertFalse($profile['first_name']['config']['required']);
+        $this->assertTrue($profile['first_name']['config']['readOnly']);
+        $this->assertTrue($profile['middle_name']['config']['readOnly']);
+        $this->assertTrue($profile['last_name']['config']['readOnly']);
+        $this->assertFalse($profile['skip_sync']['config']['readOnly']);
+        $this->assertSame('check', $profile['skip_sync']['config']['type']);
+
+        $email = $GLOBALS['TCA']['tx_academicpersons_domain_model_email']['columns']['email']['config'];
+        $this->assertSame('email', $email['type']);
+        $this->assertTrue($email['required']);
+        $this->assertFalse($GLOBALS['TCA']['tx_academicpersons_domain_model_email']['columns']['type']['config']['required']);
+
+        $address = $GLOBALS['TCA']['tx_academicpersons_domain_model_address']['columns'];
+        $this->assertTrue($address['street']['config']['required']);
+        $this->assertTrue($address['street_number']['config']['required']);
+        $this->assertTrue($address['city']['config']['required']);
+        $this->assertTrue($address['zip']['config']['required']);
+        $this->assertTrue($address['country']['config']['required']);
+        $this->assertFalse($address['additional']['config']['required']);
+
+        $phone = $GLOBALS['TCA']['tx_academicpersons_domain_model_phone_number']['columns'];
+        $this->assertTrue($phone['phone_number']['config']['required']);
+        $this->assertFalse($phone['type']['config']['required']);
+    }
+
+    /**
+     * The contract table merges the contract fields, which the `contracts`
+     * document section validates against.
+     */
+    #[Test]
+    public function contractFieldValidationIsAppliedToTheContractTable(): void
+    {
+        $contract = $GLOBALS['TCA']['tx_academicpersons_domain_model_contract']['columns'];
+
+        $this->assertTrue($contract['position']['config']['required']);
+        $this->assertTrue($contract['valid_from']['config']['required']);
+        $this->assertFalse($contract['valid_to']['config']['required']);
+        $this->assertFalse($contract['room']['config']['required']);
+        $this->assertSame('datetime', $contract['valid_from']['config']['type']);
+    }
+}

packages/fgtclb/academic-persons/Tests/Functional/Tca/ProfileInformationTcaTest.php
@@ -11,22 +11,69 @@ final class ProfileInformationTcaTest extends AbstractAcademicPersonsTestCase
 {
     /**
      * The three date columns are native SQL DATE columns whose FormEngine
-     * control is the date picker. The `[required, date]` flag list of the
-     * shipped `Settings.yaml` is merged into the `date` column's config by the
-     * TCA file, and that merge must not replace the column type: the `date`
-     * flag sets the frontend input type only.
+     * control is the date picker. The `[required, date]` flag list of every
+     * document section of the shipped `Settings.yaml` reaches the table as a
+     * `columnsOverrides` fragment of that section's record type, and that
+     * merge must not replace the column type: the `date` flag sets the
+     * frontend input type only, and a section's `required` stays with its
+     * type rather than landing on the column all seven types share.
      */
     #[Test]
     public function nativeDateTcaIsIndependentFromFrontendValidation(): void
     {
-        $columns = $GLOBALS['TCA']['tx_academicpersons_domain_model_profile_information']['columns'];
+        $table = $GLOBALS['TCA']['tx_academicpersons_domain_model_profile_information'];
         foreach (['date', 'date_start', 'date_end'] as $fieldName) {
-            $this->assertSame('datetime', $columns[$fieldName]['config']['type'], $fieldName);
-            $this->assertSame('date', $columns[$fieldName]['config']['format'], $fieldName);
-            $this->assertSame('date', $columns[$fieldName]['config']['dbType'], $fieldName);
-            $this->assertTrue($columns[$fieldName]['config']['nullable'], $fieldName);
+            $this->assertSame('datetime', $table['columns'][$fieldName]['config']['type'], $fieldName);
+            $this->assertSame('date', $table['columns'][$fieldName]['config']['format'], $fieldName);
+            $this->assertSame('date', $table['columns'][$fieldName]['config']['dbType'], $fieldName);
+            $this->assertTrue($table['columns'][$fieldName]['config']['nullable'], $fieldName);
+            $this->assertArrayNotHasKey('required', $table['columns'][$fieldName]['config'], $fieldName);
         }
-        $this->assertTrue($columns['date']['config']['required'], 'the required flag of Settings.yaml reaches the column');
-        $this->assertSame('check', $columns['year_only']['config']['type']);
+        $this->assertSame('check', $table['columns']['year_only']['config']['type']);
+        $override = $table['types']['publication']['columnsOverrides']['date']['config'];
+        $this->assertTrue($override['required'], 'the required flag of Settings.yaml reaches the record type');
+        $this->assertArrayNotHasKey('type', $override, 'the date flag does not touch the column type');
+        $this->assertArrayNotHasKey('format', $override);
+        $this->assertArrayNotHasKey('dbType', $override);
+    }
+
+    /**
+     * The seven relations of a profile to its information records are part of
+     * the domain model. They used to be generated from the settings file, so a
+     * settings override without the entry silently lost the backend column;
+     * now they are declared by the TCA file and exist whatever the settings say.
+     */
+    #[Test]
+    public function domainRelationsRemainAvailableWithoutEditSettings(): void
+    {
+        $columns = $GLOBALS['TCA']['tx_academicpersons_domain_model_profile']['columns'];
+        $expectedRelations = [
+            'scientific_research' => 'scientific_research',
+            'vita' => 'curriculum_vitae',
+            'memberships' => 'membership',
+            'cooperation' => 'cooperation',
+            'publications' => 'publication',
+            'lectures' => 'lecture',
+            'press_media' => 'press_media',
+        ];
+        foreach ($expectedRelations as $fieldName => $recordType) {
+            $this->assertSame('inline', $columns[$fieldName]['config']['type'], $fieldName);
+            $this->assertSame(
+                'tx_academicpersons_domain_model_profile_information',
+                $columns[$fieldName]['config']['foreign_table'],
+                $fieldName,
+            );
+            $this->assertSame($recordType, $columns[$fieldName]['config']['foreign_match_fields']['type'], $fieldName);
+            $this->assertSame(
+                $recordType,
+                $columns[$fieldName]['config']['overrideChildTca']['columns']['type']['config']['default'],
+                $fieldName,
+            );
+            $this->assertSame(
+                'LLL:EXT:academic_persons/Resources/Private/Language/locallang_tca.xlf:tx_academicpersons_domain_model_profile.columns.' . $fieldName . '.label',
+                $columns[$fieldName]['label'],
+                $fieldName,
+            );
+        }
     }
 }

fgtclb/academic-persons

year columns of ACE-502, with `from` and `to` aliasing `yearStart`
and `yearEnd`, and the row field of a timeline entry is `year`
The development instances are rebuilt from nothing with this commit
rather than later in the chain. The seed definition does not change
here, but what an import of it writes doe

Classes/Settings/DocumentSection.php
@@ -1,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FGTCLB\AcademicPersons\Settings;
+
+use FGTCLB\AcademicBase\Settings\ValidationSet;
+use Symfony\Component\DependencyInjection\Attribute\Exclude;
+
+/**
+ * One list of records attached to a profile: the seven profile information
+ * types, and the contracts. `type` is the record type the rows are selected
+ * by and `fieldName` the profile relation they hang off. `rowFields` and
+ * `actions` are the ordered, validated lists of what a compact row shows and
+ * offers; `readOnly` reduces the actions to viewing regardless of the list.
+ * `helptexts` maps the field keys of the section's editor - the keys of its
+ * `validators` map, aliases included - to a label reference or text.
+ *
+ * @internal not part of public API.
+ */
+#[Exclude]
+final class DocumentSection
+{
+    public const SUPPORTED_ROW_FIELDS = ['from', 'to', 'year', 'title', 'description', 'position'];
+    public const SUPPORTED_CONTRACT_ROW_FIELDS = ['from', 'to', 'position'];
+    public const SUPPORTED_PROFILE_INFORMATION_ROW_FIELDS = ['from', 'to', 'year', 'title', 'description'];
+    public const SUPPORTED_ACTIONS = ['view', 'down', 'up', 'delete', 'edit'];
+
+    /**
+     * @param list<string> $rowFields
+     * @param list<string> $actions
+     * @param array<string, string> $helptexts Per field key of the section's editor, the label reference or text
+     */
+    public function __construct(
+        public readonly string $identifier,
+        public readonly string $label,
+        public readonly string $type,
+        public readonly string $fieldName,
+        public readonly bool $readOnly,
+        public readonly ValidationSet $validationSet,
+        public readonly int $position,
+        public readonly array $rowFields = [],
+        public readonly array $actions = [],
+        public readonly array $helptexts = [],
+    ) {}
+
+    /**
+     * @param array{
+     *     identifier: string,
+     *     label: string,
+     *     type: string,
+     *     fieldName: string,
+     *     readOnly: bool,
+     *     validationSet: ValidationSet,
+     *     position: int,
+     *     rowFields?: list<string>,
+     *     actions?: list<string>,
+     *     helptexts?: array<string, string>,
+     * } $array
+     */
+    public static function __set_state(array $array): self
+    {
+        return new self(
+            identifier: $array['identifier'],
+            label: $array['label'],
+            type: $array['type'],
+            fieldName: $array['fieldName'],
+            readOnly: $array['readOnly'],
+            validationSet: $array['validationSet'],
+            position: $array['position'],
+            rowFields: $array['rowFields'] ?? [],
+            actions: $array['actions'] ?? [],
+            helptexts: $array['helptexts'] ?? [],
+        );
+    }
+
+    public function isValid(): bool
+    {
+        return $this->identifier !== ''
+            && $this->label !== ''
+            && $this->type !== ''
+            && $this->fieldName !== '';
+    }
+
+    public function isContractSection(): bool
+    {
+        return $this->identifier === 'contracts'
+            || in_array($this->type, ['contract', 'contracts'], true);
+    }
+
+    public function allowsAction(string $action): bool
+    {
+        $normalizedAction = strtolower(trim($action));
+        return in_array($normalizedAction, $this->actions, true)
+            && (!$this->readOnly || $normalizedAction === 'view');
+    }
+
+    /**
+     * @return list<string>
+     */
+    public function getAllowedActions(): array
+    {
+        return array_values(array_filter(
+            $this->actions,
+            fn(string $action): bool => $this->allowsAction($action),
+        ));
+    }
+
+    public function allowsCreate(): bool
+    {
+        return !$this->readOnly;
+    }
+
+    public function allowsDragSorting(): bool
+    {
+        return $this->allowsAction('up') && $this->allowsAction('down');
+    }
+}

Tests/Functional/Tca/EditSettingsIsolationTest.php
@@ -1,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FGTCLB\AcademicPersons\Tests\Functional\Tca;
+
+use FGTCLB\AcademicPersons\Tests\Functional\AbstractAcademicPersonsTestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+/**
+ * The section graph of `Settings.yaml` reaches the backend: every TCA file of
+ * this extension merges the set of its own section, so the flags an editor
+ * sees in the frontend are the flags the backend record editor enforces. The
+ * editing extension is deliberately not loaded here - the backend half applies
+ * without it.
+ */
+final class EditSettingsIsolationTest extends AbstractAcademicPersonsTestCase
+{
+    /**
+     * A document section addresses the records of its type only, through
+     * `columnsOverrides`, and the range of the year columns stays untouched
+     * by it.
+     */
+    #[Test]
+    public function centralDocumentValidationIsAppliedToBackendTca(): void
+    {
+        $table = $GLOBALS['TCA']['tx_academicpersons_domain_model_profile_information'];
+
+        $this->assertArrayHasKey('columnsOverrides', $table['types']['cooperation']);
+        $overrides = $table['types']['cooperation']['columnsOverrides'];
+        $this->assertTrue($overrides['title']['config']['required']);
+        $this->assertTrue($overrides['year']['config']['required']);
+        $this->assertFalse($overrides['year_start']['config']['required']);
+        $this->assertFalse($overrides['year_end']['config']['required']);
+        $this->assertArrayNotHasKey('required', $table['columns']['year']['config']);
+        $this->assertSame('number', $table['columns']['year']['config']['type']);
+        $this->assertSame('integer', $table['columns']['year']['config']['format']);
+        $this->assertSame(['lower' => 0, 'upper' => 9999], $table['columns']['year']['config']['range']);
+        $this->assertTrue($table['columns']['year']['config']['nullable']);
+    }
+
+    /**
+     * The profile table merges every profile section plus the direct special
+     * fields; the contact tables merge their own contact section each. The
+     * name fields are locked (`readonly`, `disabled`) and therefore not
+     * required, whatever else their flag list says.
+     */
+    #[Test]
+    public function centralProfileAndContactValidationIsAppliedToBackendTca(): void
+    {
+        $profile = $GLOBALS['TCA']['tx_academicpersons_domain_model_profile']['columns'];
+        $this->assertTrue($profile['gender']['config']['required']);
+        $this->assertSame(1, $profile['gender']['config']['minitems']);
+        $this->assertFalse($profile['first_name']['config']['required']);
+        $this->assertTrue($profile['first_name']['config']['readOnly']);
+        $this->assertTrue($profile['middle_name']['config']['readOnly']);
+        $this->assertTrue($profile['last_name']['config']['readOnly']);
+        $this->assertFalse($profile['skip_sync']['config']['readOnly']);
+        $this->assertSame('check', $profile['skip_sync']['config']['type']);
+
+        $email = $GLOBALS['TCA']['tx_academicpersons_domain_model_email']['columns']['email']['config'];
+        $this->assertSame('email', $email['type']);
+        $this->assertTrue($email['required']);
+        $this->assertFalse($GLOBALS['TCA']['tx_academicpersons_domain_model_email']['columns']['type']['config']['required']);
+
+        $address = $GLOBALS['TCA']['tx_academicpersons_domain_model_address']['columns'];
+        $this->assertTrue($address['street']['config']['required']);
+        $this->assertTrue($address['street_number']['config']['required']);
+        $this->assertTrue($address['city']['config']['required']);
+        $this->assertTrue($address['zip']['config']['required']);
+        $this->assertTrue($address['country']['config']['required']);
+        $this->assertFalse($address['additional']['config']['required']);
+
+        $phone = $GLOBALS['TCA']['tx_academicpersons_domain_model_phone_number']['columns'];
+        $this->assertTrue($phone['phone_number']['config']['required']);
+        $this->assertFalse($phone['type']['config']['required']);
+    }
+
+    /**
+     * The contract table merges the contract fields, which the `contracts`
+     * document section validates against.
+     */
+    #[Test]
+    public function contractFieldValidationIsAppliedToTheContractTable(): void
+    {
+        $contract = $GLOBALS['TCA']['tx_academicpersons_domain_model_contract']['columns'];
+
+        $this->assertTrue($contract['position']['config']['required']);
+        $this->assertTrue($contract['valid_from']['config']['required']);
+        $this->assertFalse($contract['valid_to']['config']['required']);
+        $this->assertFalse($contract['room']['config']['required']);
+        $this->assertSame('datetime', $contract['valid_from']['config']['type']);
+    }
+}

Tests/Functional/Tca/ProfileInformationTcaTest.php
@@ -16,23 +16,70 @@ final class ProfileInformationTcaTest extends AbstractAcademicPersonsTestCase
      * bounds, what DataHandler clamps against and what makes the derived
      * column unsigned.
      *
-     * The `[required, number]` flag list of the shipped `Settings.yaml` is
-     * merged into the `year` column's config by the TCA file. It restates
-     * neither the range nor the format, so the bounds survive the merge.
+     * The `[required, number]` flag list of every document section of the
+     * shipped `Settings.yaml` reaches the table as a `columnsOverrides`
+     * fragment of that section's record type. It restates neither the range
+     * nor the format, so the bounds survive the merge, and a section's
+     * `required` stays with its type rather than landing on the column all
+     * seven types share.
      */
     #[Test]
     public function yearColumnsCarryTheirRangeAndNoIgnoredBounds(): void
     {
-        $columns = $GLOBALS['TCA']['tx_academicpersons_domain_model_profile_information']['columns'];
+        $table = $GLOBALS['TCA']['tx_academicpersons_domain_model_profile_information'];
         foreach (['year', 'year_start', 'year_end'] as $fieldName) {
-            $config = $columns[$fieldName]['config'];
+            $config = $table['columns'][$fieldName]['config'];
             $this->assertSame('number', $config['type'], $fieldName);
             $this->assertSame('integer', $config['format'], $fieldName);
             $this->assertSame(['lower' => 0, 'upper' => 9999], $config['range'], $fieldName);
             $this->assertTrue($config['nullable'], $fieldName);
             $this->assertArrayNotHasKey('min', $config, $fieldName);
             $this->assertArrayNotHasKey('max', $config, $fieldName);
+            $this->assertArrayNotHasKey('required', $config, $fieldName);
         }
-        $this->assertTrue($columns['year']['config']['required'], 'the required flag of Settings.yaml reaches the column');
+        $override = $table['types']['publication']['columnsOverrides']['year']['config'];
+        $this->assertTrue($override['required'], 'the required flag of Settings.yaml reaches the record type');
+        $this->assertSame('number', $override['type'], 'the number flag keeps the column type');
+        $this->assertArrayNotHasKey('range', $override, 'the record type override does not restate the bounds');
+    }
+
+    /**
+     * The seven relations of a profile to its information records are part of
+     * the domain model. They used to be generated from the settings file, so a
+     * settings override without the entry silently lost the backend column;
+     * now they are declared by the TCA file and exist whatever the settings say.
+     */
+    #[Test]
+    public function domainRelationsRemainAvailableWithoutEditSettings(): void
+    {
+        $columns = $GLOBALS['TCA']['tx_academicpersons_domain_model_profile']['columns'];
+        $expectedRelations = [
+            'scientific_research' => 'scientific_research',
+            'vita' => 'curriculum_vitae',
+            'memberships' => 'membership',
+            'cooperation' => 'cooperation',
+            'publications' => 'publication',
+            'lectures' => 'lecture',
+            'press_media' => 'press_media',
+        ];
+        foreach ($expectedRelations as $fieldName => $recordType) {
+            $this->assertSame('inline', $columns[$fieldName]['config']['type'], $fieldName);
+            $this->assertSame(
+                'tx_academicpersons_domain_model_profile_information',
+                $columns[$fieldName]['config']['foreign_table'],
+                $fieldName,
+            );
+            $this->assertSame($recordType, $columns[$fieldName]['config']['foreign_match_fields']['type'], $fieldName);
+            $this->assertSame(
+                $recordType,
+                $columns[$fieldName]['config']['overrideChildTca']['columns']['type']['config']['default'],
+                $fieldName,
+            );
+            $this->assertSame(
+                'LLL:EXT:academic_persons/Resources/Private/Language/locallang_tca.xlf:tx_academicpersons_domain_model_profile.columns.' . $fieldName . '.label',
+                $columns[$fieldName]['label'],
+                $fieldName,
+            );
+        }
     }
 }